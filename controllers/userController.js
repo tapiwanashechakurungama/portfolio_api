406482cdcae9078deb7c5275b4488e8dac2514d6
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-10-16 09:48:17 +0200

    adding user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,101 @@
+import bcryptjs from "bcryptjs";
+import UserModel from "./../models/userModel.js";
+import express from "express"
+
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  try {
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json("The user with the same email already exists.");
+    } else {
+      const hashedPassword = await bcryptjs.hash(password, 10);
+      const createUser = await UserModel.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      return res.status(201).json(createUser);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validate input
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required." });
+    }
+
+    // Find user by email
+    const foundUser = await UserModel.findOne({ email });
+
+    if (foundUser) {
+      // Compare passwords
+      const passwordMatch = await bcryptjs.compare(
+        password,
+        foundUser.password
+      );
+
+      if (passwordMatch) {
+        return res.status(200).json("You logged in successfully.");
+      } else {
+        return res.status(400).json("Incorrect password.");
+      }
+    } else {
+      return res.status(404).json("User not found.");
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const allUsers = async (req, res) => {
+  try {
+    const allUsers = await UserModel.find({});
+    res.status(200).json(allUsers);
+  } catch (error) { 
+    res.status(500).json(error);
+  }
+};
+
+
+
+const updateUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await UserModel.findByIdAndUpdate(id, req.body);
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteUser = await UserModel.findByIdAndDelete(id);
+    if (deleteUser) {
+      res.status(200).json("User successfully deleted");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+export { registerUser, loginUser, allUsers, updateUserById, deleteUserById };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,109 +1,24 @@
 import express from "express";
-import bcryptjs from "bcryptjs";
-import UserModel from "../models/userModel.js";
+import { allUsers, deleteUserById, loginUser, registerUser, updateUserById } from "../controllers/userController.js";
+
 
 const router = express.Router();
 
 //register router
 
-router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: "All fields are required." });
-  }
-
-  try {
-    const existingUser = await UserModel.findOne({ email });
-    if (existingUser) {
-      return res
-        .status(400)
-        .json("The user with the same email already exists.");
-    } else {
-      const hashedPassword = await bcryptjs.hash(password, 10);
-      const createUser = await UserModel.create({
-        username,
-        email,
-        password: hashedPassword,
-      });
-      return res.status(201).json(createUser);
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-});
+router.post("/register", registerUser);
 
 //login router
 
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post("/login", loginUser);
 
-    // Validate input
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Email and password are required." });
-    }
-
-    // Find user by email
-    const foundUser = await UserModel.findOne({ email });
-
-    if (foundUser) {
-      // Compare passwords
-      const passwordMatch = await bcryptjs.compare(
-        password,
-        foundUser.password
-      );
-
-      if (passwordMatch) {
-        return res.status(200).json("You logged in successfully.");
-      } else {
-        return res.status(400).json("Incorrect password.");
-      }
-    } else {
-      return res.status(404).json("User not found.");
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-});
-
-router.get("/all", async (req, res) => {
-  try {
-    const allUsers = await UserModel.find({});
-    res.status(200).json(allUsers);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/all", allUsers);
 
 //update a specific user by id
 
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedUser = await UserModel.findByIdAndUpdate(id, req.body);
-    if (updatedUser) {
-      res.status(200).json(updatedUser);
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put("/:id", updateUserById);
 
 //delete a specific user by id
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const deleteUser = await UserModel.findByIdAndDelete(id);
-    if (deleteUser) {
-      res.status(200).json("User successfully deleted");
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", deleteUserById);
 export default router;
