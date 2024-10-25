4300b75cb400b7ff24b1a5dbd804145fef6db4b7
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-10-25 14:28:54 +0200

    api

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose"
 dotenv.config();
 
-const mongo_uri =
-  process.env.MONGO_URI ||
-  "mongodb+srv://portfolio:portfolio12@portfolio.wh3se.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
+const mongo_uri ="mongodb+srv://portfolio:portfolio12@portfolio.wh3se.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
 const connectDB = async()=>{
   try {
     await mongoose.connect(mongo_uri);
